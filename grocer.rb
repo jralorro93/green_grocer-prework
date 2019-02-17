590d3733c7a625b8cf955ae06f29b568ae84ba30
Juan Alorro <jralorro93@gmail.com>
2019-02-17 20:01:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,57 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
-end
+  con_cart = {}
+  
+  cart.each_with_index do |item, index|
+    item.each do |food, info|
+      if con_cart[food]
+        con_cart[food][:count] += 1 
+      else 
+        con_cart[food] = info
+        con_cart[food][:count] = 1 
+      end 
+    end 
+  end 
+  con_cart
+end 
+
+
+
+# def apply_coupons(cart, coupons)
+#   final = {}
+  
+#   cart.each do |item, info|
+#     coupons.each do |coupon|
+#       if item == coupon[:item] && info[:count] >= coupon[:num]
+#         info[:count] = info[:count] - coupon[:num]
+#         if final["#{item} W/ COUPON"]
+#           final["#{item} W/ COUPON"][:count] +=1 
+#         else 
+#           final["#{item} W/ COUPON"] = {:price => info[:price], :clearance => info[:clearance], :count => 1}
+#         end 
+#       end 
+#     end 
+#     final[item] = info 
+#   end 
+#   final
+# 
 
 def apply_coupons(cart, coupons)
-  # code here
-end
+  coupons.each do |coupon|
+    coupon_item = coupon[:item]
+    if cart[coupon_item] && cart[coupon_item][:count] >= coupon[:num]
+      if cart["#{coupon_item} W/ COUPON"]
+        cart["#{coupon_item} W/ COUPON"][:count] += 1 
+      else 
+        cart["#{coupon_item} W/ COUPON"] = {:count => 1, :price => coupon[:cost]}
+        cart["#{coupon_item} W/ COUPON"] [:clearance] = cart[coupon_item][:clearance]
+      end 
+      cart[coupon_item][:count] -= coupon[:num]
+    end 
+  end 
+  cart
+end 
 
 def apply_clearance(cart)
   # code here
